231ee40542bf4374c7ae7226771bf2e2a2ffdbe9
Krzysztof Cieslak <krzysztof_cieslak@windowslive.com>
2020-04-07 18:27:56 +0200

    Initial commit

---
+++ docs/loaders/globalloader.fsx
@@ -0,0 +1,22 @@
+#r "../_lib/Fornax.Core.dll"
+
+type SiteInfo = {
+    title: string
+    description: string
+    theme_variant: string option
+    numbers_in_menu: bool
+    root_url: string
+}
+
+let config = {
+    title = "Aenea"
+    description = "Description of FancyApp project"
+    theme_variant = Some "blue"
+    numbers_in_menu = true
+    root_url = "TODO: ADD_ROOT_LINK"
+}
+
+let loader (projectRoot: string) (siteContet: SiteContents) =
+    siteContet.Add(config)
+
+    siteContet
